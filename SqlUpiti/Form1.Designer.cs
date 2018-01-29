d6b6d45078ce43480ebd672aff4bc01d64a7f92f
Dijana Molnar <dijanamolnar89@gmail.com>
2018-01-29 18:02:47 +0100

    Add project files.

---
+++ SqlUpiti/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SqlUpiti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.upitLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.rezultatLabel = new System.Windows.Forms.Label();
+            this.buttonIzvrsi = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // upitLabel
+            // 
+            this.upitLabel.AutoSize = true;
+            this.upitLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.upitLabel.Location = new System.Drawing.Point(12, 20);
+            this.upitLabel.Name = "upitLabel";
+            this.upitLabel.Size = new System.Drawing.Size(144, 16);
+            this.upitLabel.TabIndex = 0;
+            this.upitLabel.Text = "SELECT SQL upit:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 46);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(390, 54);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 147);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(485, 188);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // rezultatLabel
+            // 
+            this.rezultatLabel.AutoSize = true;
+            this.rezultatLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rezultatLabel.Location = new System.Drawing.Point(15, 116);
+            this.rezultatLabel.Name = "rezultatLabel";
+            this.rezultatLabel.Size = new System.Drawing.Size(109, 16);
+            this.rezultatLabel.TabIndex = 3;
+            this.rezultatLabel.Text = "Rezultat upita:";
+            // 
+            // buttonIzvrsi
+            // 
+            this.buttonIzvrsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIzvrsi.Location = new System.Drawing.Point(426, 76);
+            this.buttonIzvrsi.Name = "buttonIzvrsi";
+            this.buttonIzvrsi.Size = new System.Drawing.Size(75, 23);
+            this.buttonIzvrsi.TabIndex = 4;
+            this.buttonIzvrsi.Text = "Izvrsi";
+            this.buttonIzvrsi.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 347);
+            this.Controls.Add(this.buttonIzvrsi);
+            this.Controls.Add(this.rezultatLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.upitLabel);
+            this.Name = "Form1";
+            this.Text = "SQL upit";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label upitLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label rezultatLabel;
+        private System.Windows.Forms.Button buttonIzvrsi;
+    }
+}
+
